8fa1f337bd75b241f52654c76a48d3eb1c600f53
Komarudin <komarudin2110@gmail.com>
2019-10-24 20:56:55 +0700

    new

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -99,7 +99,28 @@ export class EditProfile extends Component {
                     </div>
                 )
             }
-            return <h1>LOADING ....</h1>
+            return (
+                <div classname="mx-auto mt-5">
+                    <div className="spinner-grow text-primary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-secondary" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-success" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-danger" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-warning" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div className="spinner-grow text-info" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
+            )
         }
         return <Redirect to="/login" />
     }
